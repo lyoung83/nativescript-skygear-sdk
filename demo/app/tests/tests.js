0279188f7775bb499684c48a8b2a17a7e7a49b2f
LaDarius Young <lyoung@vbi.net>
2019-02-11 08:09:18 -0600

    updated unit tests

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -1,12 +1,30 @@
 var SkygearSdk = require("nativescript-skygear-sdk").SkygearSdk;
-var skygearSdk = new SkygearSdk();
+var skygearSdk = new SkygearSdk({ apiKey: "plugintest123", address: "http://localhost:3001/" });
 
-describe("greet function", function() {
+describe("version function", function() {
     it("exists", function() {
-        expect(skygearSdk.greet).toBeDefined();
+        expect(skygearSdk.version).toBeDefined();
     });
 
     it("returns a string", function() {
-        expect(skygearSdk.greet()).toEqual("Hello, NS");
+        expect(skygearSdk.version()).toEqual("0.0.1");
+    });
+
+    it("container exists", function() {
+        expect(skygearSdk.getContainer()).not.toBeUndefined();
     });
 });
+
+describe("database functions", function() {
+    it("exists", function() {
+        expect(skygearSdk.db).toBeDefined();
+    });
+
+    it("private database exists", function(){
+        expect(skygearSdk.db.getPrivateDatabase()).not.toBeUndefined()
+    })
+
+    it("public database exists", function(){
+        expect(typeof skygearSdk.db.getPublicDatabase()).not.toBeUndefined()
+    })
+})
